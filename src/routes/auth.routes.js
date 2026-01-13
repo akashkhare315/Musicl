f14571cb8b7eceec6792aeeda8d9a011374f9acf
akashkhare315 <your-email@domain.com>
2026-01-13 21:52:42 +0530

    validate register

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const registerController = require("../controllers/auth.controller");
+const {registerValidationRules} = require("../middleware/validation.middleware")
 
-router.post("/register", registerController.register);
+router.post("/register", registerValidationRules, registerController.register);
 
 module.exports = router;
